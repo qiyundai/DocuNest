4f302c77c2d48709f93bc24574050ea47e844ae7
Qiyun Dai <pianoquin@gmail.com>
2025-10-06 17:44:47 -0400

    chore: prepare repo for pnpm

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['node_modules', 'dist', 'build', '.next', 'coverage'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'unused-imports/no-unused-imports': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
+  }
+};
---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'prettier'],
+  rules: { '@next/next/no-img-element': 'off' }
+};
